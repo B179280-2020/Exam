6537d9f14d3200fa3409b3436afdcb65c85170b9
B179280-2020 <email_not_provided>
2020-12-15 00:04:45 +0000

    Added some codes to run BLAST

--- exam.py
+++ exam.py
@@ -92,3 +92,45 @@ if choice1 == "protein":
 	mdb = "makeblastdb -in protein_seq.fa -dbtype prot -out " + taxon_gp
 #print(mdb)
 subprocess.call(mdb,shell=True)
+#asking the user to determine the query sequence for BLAST analysis
+print("You may have 2 options to determine the query sequence. 1: enter a Seq ID; 2: enter the whole sequence")
+
+def check_ID(ID):     
+	#when you do esearch it print out ENTREZ_DIRECT, where you can find found data count, I try to save that value and check
+	if choice1 == "nucleotide":
+		count = subprocess.check_output('esearch -db nucleotide -query "{0}" | xtract -pattern ENTREZ_DIRECT -element Count'.format(ID),shell=True) 
+	if choice1 == "protein":
+		count = subprocess.check_output('esearch -db protein -query "{0}" | xtract -pattern ENTREZ_DIRECT -element Count'.format(ID),shell=True)
+	if int(count) == 0:      #if there is no data found thenfunctionss return 0 further it gaves error message to the user
+			return 0
+	return 1         #if there is at least one data found regarding to the user input we continue p
+
+choice4 = input("Please enter your choice. 1/2\n")
+if choice4 == "1":
+	ID = input("Please enter a Seq ID that you would like to do BLAST\n")
+	while check_ID(ID) != 1:
+		ID = input("Sorry, your Seq ID may not be correct. Please enter the correct one!")
+	print("Thanks, the sequence you have chosen will be downloaded and served as the test sequence for BLAST.")
+	if choice1 == "nucleotide":
+		os.system("esearch -db nucleotide -query " + ID + " | efetch -db nucleotide -format fasta > test_seq.fa")
+		blt = "blastp -db " + taxon_gp + " -query test_seq.fa -outfmt 6 > blastoutput.out"
+		#print(blt)
+		subprocess.call(blt,shell=True)
+		print("BLAST analysis has been successfully done!")
+	if choice1 == "protein":
+		os.system("esearch -db protein -query " + ID + " | efetch -db protein -format fasta > test_seq.fa")
+		blt = "blastp -db " + taxon_gp + " -query test_seq.fa -outfmt 6 > blastoutput.out"
+		#print(blt)
+		subprocess.call(blt,shell=True)
+		print("BLAST analysis has been successfully done!")
+if choice4 == "2":
+	iseq = input("Please enter the whole sequence")
+	print("Thanks, we will use this sequence to do BLAST!")
+	seq_of_interest = open('seq_of_interest.fa',"w")                 #creat a new file for storing the motifs information, make it writable
+	seq_of_interest.write(">Sequence_of_Interest\n")
+	seq_of_interest.write('{0}'.format(iseq))  
+	seq_of_interest.close()
+	blt = "blastp -db " + taxon_gp + " -query seq_of_interest.fa -outfmt 6 > blastoutput.out"
+	#print(blt)
+	subprocess.call(blt,shell=True)
+	print("BLAST analysis has been successfully done!")
